4b6d90fab20402442445e8795f9f3b2375cd962b
guilherme.nascimento <guiedunascimemto@gmail.com>
2024-04-01 10:26:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+        <title>javascript hello wordl!</title>
+
+<script>
+    function main() {
+        alert("hello world ")         
+    }
+    </script>
+</head>
+<body>
+
+</body>
+</html>
+
